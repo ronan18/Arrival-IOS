8e35b3a3f2235c4e8d91167fe24090f9002f5e19
Ronan <rsf.sho@gmail.com>
2020-01-07 09:10:57 -0800

    added location discovery

--- Arrival/Models/Coordinate.swift
+++ Arrival/Models/Coordinate.swift
@@ -8,5 +8,24 @@
 
 import Foundation
 import CoreLocation
-let locationManager = CLLocationManager()
+
+class Coordinate: NSObject, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager!
+    override init()      {
+        super.init()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            if let location = locations.first {
+                print(location.coordinate)
+            }
+        }
+    }
+    
+    
+    
+}
 
--- Arrival/userData.swift
+++ Arrival/userData.swift
@@ -14,22 +14,50 @@ import CoreLocation
 //let baseURL = "http://localhost:3000"
 let baseURL = "https://api.arrival.city"
 
-final class UserData: ObservableObject {
-    @Published var authorized = false
-    @Published var passphrase = ""
+
+final class UserData: NSObject, CLLocationManagerDelegate, ObservableObject {
+    @Published var authorized = true
+    //@Published var passphrase = ""
+    @Published var passphrase = "test"
     @Published var trains = trainData
     @Published var dataLoaded: Bool = false
     @Published var stations = [Station]()
     @Published var network: Bool = true
-    let locationManager = CLLocationManager()
+    @Published var lat: Double = 0.0
+    @Published var long: Double = 0.0
+    let locationManager: CLLocationManager = CLLocationManager()
     
-    init() {
+    override init() {
+        super.init()
         print("init")
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            print("location auth")
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        } else {
+            print("no location auth")
+        }
+        
         getStations()
         if self.authorized {
             beginHome()
         }
         
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else {
+            return  print("errorlocation?")
+        }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        self.lat = locValue.latitude
+        self.lat = locValue.longitude
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if(status == CLAuthorizationStatus.denied) {
+            print("no location access")
+        }
     }
     func beginHome() {
         print("running home fetches")
@@ -38,19 +66,21 @@ final class UserData: ObservableObject {
         }
     }
     func getNearestStation() {
+        print("getting nearest station")
+        
         
         
         
     }
+    
     func getStations (){
         
         Alamofire.request(baseAPI + "/api/v2/stations")
             .responseJSON{
                 response in
                 
-                let responseJSON = JSON(response)
-                print("spacer")
-                print(response, "response status")
+                //  print("spacer")
+                //  print(response, "response status")
                 if  response.value != nil {
                     let stationJSON = JSON(response.value)
                     for i in 0..<stationJSON.count {
@@ -62,7 +92,8 @@ final class UserData: ObservableObject {
                         let long = json["gtfs_longitude"].doubleValue
                         self.stations.append(Station(id:id, name: name, lat: lat, long: long, abbr: abbr))
                     }
-                    print(self.stations)
+                    //  print(self.stations)
+                    print("got stations")
                 } else {
                     print("error retreving stations")
                     self.network = false
