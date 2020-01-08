dc32995c8177620d00420f8d0f69ce7f062b073e
Ronan <rsf.sho@gmail.com>
2020-01-08 06:35:50 -0800

    removed excess functions

--- Arrival/userData.swift
+++ Arrival/userData.swift
@@ -16,29 +16,6 @@ var container: NSPersistentContainer!
 //let baseURL = "http://localhost:3000"
 let baseURL = "https://api.arrival.city"
 
-func deg2rad(deg:Double) -> Double {
-    return deg * M_PI / 180
-}
-
-func rad2deg(rad:Double) -> Double {
-    return rad * 180.0 / M_PI
-}
-
-func distance(lat1:Double, lon1:Double, lat2:Double, lon2:Double, unit:String) -> Double {
-    let theta = lon1 - lon2
-    var dist = sin(deg2rad(deg: lat1)) * sin(deg2rad(deg: lat2)) + cos(deg2rad(deg: lat1)) * cos(deg2rad(deg: lat2)) * cos(deg2rad(deg: theta))
-    dist = acos(dist)
-    dist = rad2deg(rad: dist)
-    dist = dist * 60 * 1.1515
-    if (unit == "K") {
-        dist = dist * 1.609344
-    }
-    else if (unit == "N") {
-        dist = dist * 0.8684
-    }
-    return dist
-}
-
 let appDelegate = UIApplication.shared.delegate as! AppDelegate
 let context = appDelegate.persistentContainer.viewContext
 final class UserData: NSObject, CLLocationManagerDelegate, ObservableObject {
