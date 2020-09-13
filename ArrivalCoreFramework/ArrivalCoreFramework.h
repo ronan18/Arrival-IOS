234ab2e5fde595ee2cd4f17f1dadadca340aa7b5
Ronan <rsf.sho@gmail.com>
2020-09-13 09:53:39 -0700

    pre framework

---
+++ ArrivalCoreFramework/ArrivalCoreFramework.h
@@ -0,0 +1,19 @@
+//
+//  ArrivalCoreFramework.h
+//  ArrivalCoreFramework
+//
+//  Created by Ronan Furuta on 9/13/20.
+//  Copyright © 2020 Stomp Rocket. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArrivalCoreFramework.
+FOUNDATION_EXPORT double ArrivalCoreFrameworkVersionNumber;
+
+//! Project version string for ArrivalCoreFramework.
+FOUNDATION_EXPORT const unsigned char ArrivalCoreFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArrivalCoreFramework/PublicHeader.h>
+
+
--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'Arrival-iOS2' do
+target 'ArrivalCoreFramework' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
   
