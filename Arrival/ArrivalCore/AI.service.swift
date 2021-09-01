b215717b42a885168b76f12d3eec5dffc7ef717f
Ronan <rsf.sho@gmail.com>
2021-08-31 19:57:39 -0700

    macOS build target

---
+++ Arrival/ArrivalCore/AI.service.swift
@@ -0,0 +1,22 @@
+//
+//  AI.service.swift
+//  AI.service
+//
+//  Created by Ronan Furuta on 8/31/21.
+//
+
+import Foundation
+import TabularData
+import CoreML
+#if !DEBUG
+import CreateML
+
+public class AIService {
+    public init () {
+        
+    }
+    public func train() {
+    
+    }
+}
+#endif
