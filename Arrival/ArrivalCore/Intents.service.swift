9a8ca4bef1ac5a3447bff924b55a1d56ca237994
Ronan <ronan@ronanfuruta.com>
2021-09-03 13:41:13 -0700

    Intents service class start

---
+++ Arrival/ArrivalCore/Intents.service.swift
@@ -0,0 +1,16 @@
+//
+//  Intents.service.swift
+//  Intents.service
+//
+//  Created by Ronan Furuta on 9/3/21.
+//
+
+import Foundation
+import Siri_Intents
+
+public class IntentsService {
+    public init () {
+        
+    }
+    
+}
